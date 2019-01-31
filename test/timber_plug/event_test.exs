c68e994248a7d63c7a4813cde11490ce860d191d
Mitchell Henke <mitchh23@gmail.com>
2019-01-31 16:13:11 -0600

    add test for event plug

--- test/timber_plug/event_test.exs
+++ test/timber_plug/event_test.exs
@@ -1,3 +1,21 @@
 defmodule Timber.Plug.EventTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use Plug.Test
+  import ExUnit.CaptureLog
+
+  defp call(conn, opts) do
+    Timber.Plug.Event.call(conn, Timber.Plug.Event.init(opts))
+  end
+
+  test "logs request received and response sent" do
+    log =
+      capture_log(fn ->
+        conn(:get, "/")
+        |> call(log_level: :error)
+        |> send_resp(200, "")
+      end)
+
+    assert log =~ "Received GET /"
+    assert log =~ "Sent 200 response in"
+  end
 end
